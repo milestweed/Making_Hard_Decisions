575a43d842bc77f2855077ad21714fddd7c26741
Miles Tweed <milestweed@gmail.com>
2021-10-28 14:29:27 -0400

    Debug Off

--- sc_proj/sc_proj/settings.py
+++ sc_proj/sc_proj/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = get_secret()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
